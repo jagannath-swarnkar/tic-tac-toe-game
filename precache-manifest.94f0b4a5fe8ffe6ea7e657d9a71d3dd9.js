f58c8590a4f11c7cd10909eb7672dfc7452f34ee
jagannath-swarnkar <jagannath18@navgurukul.org>
2019-09-25 20:22:36 +0530

    Updates

---
+++ precache-manifest.94f0b4a5fe8ffe6ea7e657d9a71d3dd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ec509cb8e88173b208135145baaead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f97d6d9058bbc43e583",
+    "url": "/static/css/main.413d4f3c.chunk.css"
+  },
+  {
+    "revision": "e6c1c8651bda8a8c6785",
+    "url": "/static/js/2.d8c4d936.chunk.js"
+  },
+  {
+    "revision": "9f97d6d9058bbc43e583",
+    "url": "/static/js/main.f2c9b9ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
