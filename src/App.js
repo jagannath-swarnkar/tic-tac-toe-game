9ee505dd666c4622ded84ad3b2c2d2f2e6c08538
jagannath-swarnkar <jagannath18@navgurukul.org>
2019-09-25 23:16:36 +0530

    adding .gitignore file updating all files with new version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React,{Component} from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+
+
 import './App.css';
 
 class App extends Component{
@@ -9,13 +13,15 @@ class App extends Component{
       user:"X",
       userX:[],
       userO:[],
-      winner:""
+      winner:"",
+      count:1
     }
     this.game = this.game.bind(this);
     this.winner = this.winner.bind(this);
-    var winnerInstance = this.state.winner;
   }
-  winner(){
+  winner(drow){    
+    var count=this.state.count;
+    this.setState({count:count+1})
     var list=[["0","1","2"],["3","4","5"],["6","7","8"],["0","3","6"],["1","4","7"],["2","5","8"],["0","4","8"],["2","4","6"]];
     for(var i in list){
       var countX = 0;
@@ -31,25 +37,32 @@ class App extends Component{
           break;
         }
       }
-      if(countX==3){
+      if(countX===3){
         this.setState({winner:"X"})
         document.getElementById('userguide').style.display="none";
         document.getElementById('userwinner').style.display="block";
-      }else if(countO==3){
+        document.getElementById('drow').style.display="none";
+      }else if(countO===3){
         this.setState({winner:"O"})
         document.getElementById('userguide').style.display="none";
         document.getElementById('userwinner').style.display="block";
+        document.getElementById('drow').style.display="none";
+      }else if(drow && this.state.winner===""){
+      
+        document.getElementById('userguide').style.display="none";
+        document.getElementById('drow').style.display="block";
       }
     }
   }
   game(event){
+    
     var id = event.target.id;
     var itemInstance = this.state.item;
     var userXIns = this.state.userX;
     var userOIns = this.state.userO;
     var winnerInstance = this.state.winner;
 
-    if((this.state.user=="X") && (!userXIns.includes(id))&& (!userOIns.includes(id)) && winnerInstance==""){
+    if((this.state.user==="X") && (!userXIns.includes(id))&& (!userOIns.includes(id)) && winnerInstance===""){
       itemInstance[id]="X";
       userXIns.push(id);
       this.setState({
@@ -59,7 +72,7 @@ class App extends Component{
       })
       this.winner();
     }
-    else if((this.state.user=="O") && (!userOIns.includes(id)) && (!userXIns.includes(id)) && winnerInstance==""){
+    else if((this.state.user==="O") && (!userOIns.includes(id)) && (!userXIns.includes(id)) && winnerInstance===""){
       itemInstance[id]="O";
       userOIns.push(id);
       this.setState({
@@ -68,17 +81,22 @@ class App extends Component{
         userO:userOIns
       })
       this.winner();
-    }
+    }if(this.state.count===9){console.log('this.state.count')
+      console.log('hello');
+      
+      var drow=true
+      this.winner(drow);  
+    }console.log(this.state.count)
   }
   Restart(){
     document.location.reload()
   }
   render(){
 
-    return(
+    return(<Card>
       <div className="structure">
         <h2 id="userguide">Next Player : {this.state.user}</h2>
-        <table>
+        <table><tbody>
           <tr>
             <td id="0" onClick={this.game}>{this.state.item[0]}</td>
             <td id="1" onClick={this.game}>{this.state.item[1]}</td>
@@ -94,10 +112,19 @@ class App extends Component{
             <td id="7" onClick={this.game}>{this.state.item[7]}</td>
             <td id="8" onClick={this.game}>{this.state.item[8]}</td>
           </tr>
-        </table>
-        <h2><button onClick={this.Restart}>Restart</button></h2>
-        <h2 id="userwinner">Congratulation ! <br/><span>Your Winner : {this.state.winner}</span></h2>
-      </div>
+          </tbody></table>
+        <h2>
+          {/* <button onClick={this.Restart}>Restart</button> */}
+          <Button variant="contained" 
+            color="primary" 
+            fullWidth 
+            onClick={this.Restart}>
+            Restart
+          </Button>
+        </h2>
+        <h2 id="userwinner">Congratulation ! <br/><span style={{color:'green'}}>Your Winner : {this.state.winner}</span></h2>
+        <h2 id="drow">Oops ! <br/><span style={{color:'green'}}>Match draw ! Please try again.</span></h2>
+        </div></Card>
     )
   }
 
